bbd3c3f4a1083708bc60787dc1c04b1c6ae636d8
ryan-lachance <r-lachance@hotmail.com>
2024-03-12 16:07:34 -0400

    Setup simple navbar for ease of development.

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from "react";
+
+function About() {
+  return (
+    <div>
+        <p>Test2</p>
+    </div>
+  )
+  ;
+}
+
+export default About
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+        <p>Test</p>
+    </div>
+  )
+  ;
+}
+
+export default Home
