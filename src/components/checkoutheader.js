315352aefbab547562c66432c68900c9c4178aac
ryan-lachance <r-lachance@hotmail.com>
2024-03-22 20:52:45 -0400

    progress

--- src/components/checkoutheader.js
+++ src/components/checkoutheader.js
@@ -13,16 +13,23 @@ function Checkoutheader({progress, back, next}) {
     navigate(back); // Replace '/book' with the path you want to navigate to
   };
 
+  const goHome = () => {
+    navigate('/'); // Replace '/book' with the path you want to navigate to
+  };
+
   return (  
-
-  <div className="flex items-center mx-4 space-x-2"> {/* Adjust mx-4 to control the spacing from the screen edges */}
-    <button className="nav-btn" onClick={goBack}>Back</button>
-    <div className="flex-1 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-      <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${progress}%` }}></div>
+  <div>
+    <div className="flex justify-start w-full">
+      <button onClick={goHome}  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded-full m-2">Home</button>
     </div>
-    <button className="nav-btn" onClick={goNext}>Next</button>
+    <div className="flex items-center mx-4 space-x-2"> {/* Adjust mx-4 to control the spacing from the screen edges */}
+      <button className="nav-btn" onClick={goBack}>Back</button>
+      <div className="flex-1 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+        <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${progress}%` }}></div>
+      </div>
+      <button className="nav-btn" onClick={goNext}>Next</button>
+    </div>
   </div>
-
 
   );
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -12,11 +12,48 @@ function Checkout() {
 
         <div className="flex h-screen">
   <div name='services' className='flex-grow p-4 border border-gray-200' style={{ flexBasis: '75%' }}>
-    {/* Services content */}
+    <p>Personal Information</p>
+  <div className="flex w-full">
+  <div className="flex-1 ">
+      <label for="first_name" class="block mb-2 text-sm font-medium text-black">First name</label>
+      <input type="text" id="first_name" class="input" required />
+      <label for="last_name" class="block mb-2 text-sm font-medium text-black">Last name</label>
+      <input type="text" id="last_name" class="input" required />
+      <label for="address" class="block mb-2 text-sm font-medium text-black">Address</label>
+      <input type="text" id="address" class="input" required />
+      <label for="province" class="block mb-2 text-sm font-medium text-black">Province</label>
+      <input type="text" id="province" class="input" required />
+
+    </div>
+    <div className="flex-1">
+      <label for="email" class="block mb-2 text-sm font-medium text-black">Email</label>
+      <input type="text" id="email" class="input" required />
+      <label for="phone" class="block mb-2 text-sm font-medium text-black">Phone Number</label>
+      <input type="tel" id="phone" class="input" required />
+      <label for="postal" class="block mb-2 text-sm font-medium text-black">Postal Code</label>
+      <input type="text" id="postal" class="input" required />
+    </div>
+    <div className="flex-1">
+      <label for="credit" class="block mb-2 text-sm font-medium text-black">Credit Card Number</label>
+      <input type="text" id="credit" class="input" required />
+      <label for="exp" class="block mb-2 text-sm font-medium text-black">Credit Card Expiration Date</label>
+      <input type="date" id="exp" class="input" required />
+      <label for="security" class="block mb-2 text-sm font-medium text-black">Credit Card Security Code</label>
+      <input type="text" id="security" class="input" required />
+      <label for="card_name" class="block mb-2 text-sm font-medium text-black">Name on Card</label>
+      <input type="text" id="card_name" class="input" required />
+    </div>
+  </div>
   </div>
 
   <div name='stylists' className='flex-grow p-4 border border-gray-200' style={{ flexBasis: '25%' }}>
-    {/* Stylists content */}
+    <p>Price</p>
+    <p className="text-left font-bold my-8">Service Fee:</p>
+    <p className="text-left font-bold my-8">Products Fee:</p>
+    <p className="text-left font-bold my-8">Tax:</p>
+    <p className="text-left font-bold my-8">Total:</p>
+    <button className="btn">Checkout</button>
+
     <br/>
   </div>
 </div>
